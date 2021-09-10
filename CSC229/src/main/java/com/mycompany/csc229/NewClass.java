53e1ef9f4f3aac80a1b1251ad067a59162c19f29
chriscanenguez <Christopher Canenguez>
2021-09-10 12:26:30 -0400

    NewClass Commit

--- CSC229/src/main/java/com/mycompany/csc229/NewClass.java
+++ CSC229/src/main/java/com/mycompany/csc229/NewClass.java
@@ -9,6 +9,29 @@ package com.mycompany.csc229;
  *
  * @author chriscanenguez
  */
-public class NewClass {
+public class NewClass 
+{
+    int id;
+    NewClass nxt;
+    NewClass (int id) {this.id = id;}
     
+    @Override
+    public String toString()
+    {
+        return "Class id is " + this.id;
+    }
+    
+    public static void main(String[] args)
+    {
+        NewClass c1 = new NewClass(1);
+        NewClass c2 = new NewClass(2);
+        c1.nxt = c2;// This is called a linked list.
+        NewClass c3 = new NewClass(3);
+        c2.nxt = c3;
+        NewClass c4 = new NewClass(4);
+        NewClass c5 = new NewClass(5);
+        
+        System.out.println(c1);
+    }
+          
 }
